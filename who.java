ce632e128379bac814bc31239c73cdcafa3bbe7f
Paramilk <82511910+Paramilk@users.noreply.github.com>
2021-09-06 01:56:32 +0100

    Add files via upload

---
+++ who.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class who{ 
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What's your name?");
+        String name = input.nextLine();
+        if (name.equals("Walter")) {
+            System.out.println("Hello there, You Sussy Baka!");}
+            else{ 
+            System.out.println("Hello there, Fellow Crewmates");}            
+
+            
+        }
+    }
